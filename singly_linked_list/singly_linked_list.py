11317efcf0d2b4da8db83e84d5159a449d33599e
aland1013 <adam.land@gmail.com>
2020-09-15 23:44:32 -0400

    cleanup

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -2,63 +2,70 @@ class Node:
     def __init__(self, value=None):
         self.value = value
         self.next = None
-
-class LinkedList:
-    def __init__(self, node=None):
-        self.head = node
-        self.tail = node
-        self.length = 1 if node is not None else 0
-
-    def __len__(self):
-        return self.length
     
+    def get_value(self):
+        """
+        Method to get the value of a node
+        """
+        return self.value
+    
+    def get_next(self):
+        """
+        Method to get the node's next node
+        """
+        return self.next
+    
+    def set_next(self, new_next):
+        """
+        Method to update the node's next node
+        """
+        self.next = new_next
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
     def add_to_tail(self, value):
         new_node = Node(value)
-        self.length += 1
         
-        if self.length == 1:
+        if self.head is None and self.tail is None:
             self.head = new_node
             self.tail = new_node
         
-        else:        
-            old_tail = self.tail
-            old_tail.next = new_node
+        else:
+            self.tail.set_next(new_node)
             self.tail = new_node
     
-    def remove_head(self):
-        if self.length == 0:
-            return None
-        
-        elif self.length == 1:
-            self.length -=1
-            value = self.head.value
-            self.head = None
-            self.tail = None
-            return value
-    
-        else:
-            self.length -= 1
-            old_head = self.head
-            self.head = old_head.next
-            return old_head.value
-    
     def remove_tail(self):
-        if self.length == 0:
+        if self.head is None and self.tail is None:
             return None
         
-        elif self.length == 1:
-            self.length -= 1
-            value = self.tail.value
+        if self.head == self.tail:
+            value = self.head.get_value()
             self.head = None
             self.tail = None
             return value
         
-        else:
-            self.length -= 1
-            old_tail = self.tail
-            new_tail = self.head
-            while new_tail.next != old_tail:
-                new_tail = new_tail.next
-            self.tail = new_tail
-            return old_tail.value
+        value = self.tail.get_value()
+        new_tail = self.head
+        while new_tail.get_next() != self.tail:
+            new_tail = new_tail.get_next()
+        self.tail = new_tail
+        self.tail.set_next(None)
+        return value
+    
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return None
+        
+        if self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        
+        value = self.head.get_value()
+        self.head = self.head.get_next()
+        return value
             
